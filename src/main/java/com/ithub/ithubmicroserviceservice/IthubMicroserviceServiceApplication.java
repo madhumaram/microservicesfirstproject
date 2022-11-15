39fccc165a481f14ac75fc6975f28a8a77030111
madhumaram <30398183+madhumaram@users.noreply.github.com>
2022-11-14 19:06:46 -0500

    initial commit

---
+++ src/main/java/com/ithub/ithubmicroserviceservice/IthubMicroserviceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ithub.ithubmicroserviceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthubMicroserviceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IthubMicroserviceServiceApplication.class, args);
+	}
+
+}
